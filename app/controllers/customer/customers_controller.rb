2c1750ea80cd6aa12eaa45c26e3204fceef473c1
Hazuki083 <ha882ki@gmail.com>
2021-08-13 14:37:43 +0000

    マイページ製作中

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,14 +1,41 @@
 class Customer::CustomersController < ApplicationController
-  
+   before_action :authenticate_customer!
+
   def index
   end
-  
+
   def show
+    @customer = Customer.find(current_customer.id)
   end
   
+  def quit
+    @customer = Customer.find(current_customer.id)
+  end
+  
+  def out
+  end
+
+
   def edit
+    @customer = Customer.find(current_customer.id)
+    if @customer != current_customer
+       redirect_to customer_path(current_customer.id)
+    end
+  end
+
+  def update
+    @customer = Customer.find(current_customer.id)
+    if @customer.update(customer_params)
+       flash[:notice] = "You have updated user successfully."
+       redirect_to user_path(@user.id)
+    else
+       render :edit
+    end
   end
   
-  def update
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:name, :age, :skin, :email, :image_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
   resources :customers, only: [:index, :show, :edit, :update]
   resources :items, only: [:index, :show]
   resources :posts
+  
  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
