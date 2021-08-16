8a1d1b89a0a39c50755f4e10ac2a51848fc8f263
Hazuki083 <ha882ki@gmail.com>
2021-08-16 11:58:34 +0000

    デプロイセッティング

--- app/controllers/admin/admins/unlocks_controller.rb
+++ app/controllers/admin/admins/unlocks_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Admins::UnlocksController < Devise::UnlocksController
+# class Admins::UnlocksController < Devise::UnlocksController
   # GET /resource/unlock/new
   # def new
   #   super
@@ -27,4 +27,4 @@ class Admins::UnlocksController < Devise::UnlocksController
   # def after_unlock_path_for(resource)
   #   super(resource)
   # end
-end
+# end
