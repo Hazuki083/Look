7569daec6ffd5d84542136701e115b61051647f2
Hazuki083 <ha882ki@gmail.com>
2021-08-24 12:27:57 +0000

    年代追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -9,5 +9,6 @@ class Customer < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :items, through: :likes  #throughを使ってモデル同士のアソシエーション（多：多）
-
+ 
+ enum age: { '10代': 10, '20代': 20, '30代': 30, '40代': 40, '50代': 50, '非公開': 0 }
 end
