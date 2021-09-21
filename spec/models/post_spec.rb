81a60f1dd90091279e325b3dc2fc0ba482d6a86f
Hazuki083 <ha882ki@gmail.com>
2021-09-21 12:42:25 +0000

    RSpec test postmodel

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,26 +5,19 @@ RSpec.describe 'Postモデルのテスト', type: :model do
      # factoriesで作成したダミーデータを使用
     let(:customer) { FactoryBot.create(:customer) }
     let!(:post) { build(:post, customer_id: customer.id) }
-    
+     # インスタンス変数の代わりにletを使う
      # test_postを作成し、空欄での登録ができるか確認する
     subject { test_post.valid? }
     let(:test_post) { post }
+
      context 'reviewカラム' do
       it '空欄でないこと' do
-        test_post.title = ''
+        test_post.review = ''
         is_expected.to eq false;
-      end
-       it '200文字以下であること: 200文字は〇' do
-        post.review = Faker::Lorem.characters(number: 200)
-        is_expected.to eq true
-      end
-      it '200文字以下であること: 201文字は×' do
-        post.review = Faker::Lorem.characters(number: 201)
-        is_expected.to eq false
       end
     end
   end
-  
+
   describe 'アソシエーションのテスト' do
     context 'customerモデルとの関係' do
       it 'N:1となっている' do
